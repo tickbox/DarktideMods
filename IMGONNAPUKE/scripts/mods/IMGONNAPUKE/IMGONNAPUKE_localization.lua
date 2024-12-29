c7b7c1dd28a3d28a010d1d51f119b048743914c1
tickbox <tickbox@gmail.com>
2024-12-29 10:40:26 -0500

    Added subtitles and support for future localization.

--- IMGONNAPUKE/scripts/mods/IMGONNAPUKE/IMGONNAPUKE_localization.lua
+++ IMGONNAPUKE/scripts/mods/IMGONNAPUKE/IMGONNAPUKE_localization.lua
@@ -1,5 +1,53 @@
 return {
+	mod_name = {
+		en = "IMGONNAPUKE",
+	},
 	mod_description = {
-		en = "IMGONNAPUKE description",
+		en = "PUKE SETTINGS",
+	},
+	puke_volume = {
+		en = "PUKE VOLUME",
+	},
+	puke_frequency = {
+		en = "PUKE FREQUENCY",
+	},
+	puke_group = {
+		en = "PUKE TOGGLE",
+	},
+	setting_puke1 = {
+		en = "IM GONNA FUCKIN PUKE",
+	},
+	subtitle_puke1 = {
+		en = "MCCONNELL: I'M GONNA FUCKIN' PUKE!!"
+	},
+	setting_puke2 = {
+		en = "IM GONNA PUKE IM GONNA PUKE",
+	},
+	subtitle_puke2 = {
+		en = "MCCONNELL: I'M GONNA PUKE! I'M GONNA PUKE!"
+	},
+	setting_puke3 = {
+		en = "IM GONNA FUCKIN THROW UP",
+	},
+	subtitle_puke3 = {
+		en = "PENTA: I'M GONNA FUCKIN' THROW UP!"
+	},
+	setting_puke4 = {
+		en = "IM GONNA FUCKING PUKE PLEASE",
+	},
+	subtitle_puke4 = {
+		en = "MCCONNELL: I'M GONNA FUCKIN' PUKE! I'M THROWIN' UP! DUDE, PLEASE?!"
+	},
+	setting_puke5 = {
+		en = "BLUH BLUH BLUH BLUH",
+	},
+	subtitle_puke5 = {
+		en = "MCCONNELL: (BLUH BLUH BLUH BLUH)"
+	},
+	puke_subtitles = {
+		en = "ENABLE SUBTITLES",
+	},
+	puke_subtitle_duration = {
+		en = "SUBTITLE DURATION",
 	},
 }
